f01b1fdb414f6131bd06b9f4156a4752ba286cc3
manturoy440 <mantukumar823512@gmail.com>
2025-09-18 19:49:40 +0530

    cerated model for user and vendor

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,43 +1,16 @@
 import mongoose from "mongoose";
 
+const userSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true, lowercase: true, trim: true },
+    password: { type: String, required: true },
+    vendor: [{ type: mongoose.Schema.Types.ObjectId, ref: "Vendor" }],
+   
+  },
+  { timestamps: true }
+);
 
-const userSchema=  new mongoose.Schema({
-    firstName:{
-        type:String,
-        required:true,
-       
-    },
-    lastName:{
-        type:String,
-        required:true
-    },
-    email:{
-        type:String,
-        required:true,
-        
+const User = mongoose.model("User", userSchema);
 
-    },
-    password:{
-        type:String,
-        required:true
-    },
-    role:{
-        type:String,
-        enum:['couple','wendor','admin'],
-        required:true,
-    },
-    profilePicture:String,
-    phoneNumber:String,
-    location:{
-        city:String,
-        state:String,
-        country:String
-    },
-    createdAt:{
-        type:Date,
-        default:Date.now
-    }
-
-})
-
-export default mongoose.model('User',userSchema);
+export default User;
